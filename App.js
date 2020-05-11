83bc84b090a1833deab73fb69510bd7f567aedd4
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-11 04:48:35 +0100

    delete package.lock

--- App.js
+++ App.js
@@ -3,25 +3,9 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          placeholder="Enter Goals"
-          style={{
-            borderBottomColor: "black",
-            borderBottomWidth: 1,
-            padding: 5,
-            width: "60%",
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter Goals" style={styles.input} />
         <Button title="Add Goals" />
       </View>
 
@@ -30,4 +14,21 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    padding: 5,
+    width: "60%",
+  },
+});
