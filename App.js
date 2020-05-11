a484f6b847f76174f94514c4e0b039dc039bf727
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-11 04:25:51 +0100

    create react native app

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Enter Goals"
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            padding: 5,
+            width: "60%",
+          }}
+        />
+        <Button title="Add Goals" />
+      </View>
+
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
