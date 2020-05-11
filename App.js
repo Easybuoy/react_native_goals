4e00410e04f4405c1326b3994b88d549c561a8d5
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-11 04:58:31 +0100

    add events to inuput

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = (text) => {
+    setGoal(text);
+  };
+
+  const addGoalHandler = () => {
+    console.log(goal);
+    setGoals([...goals, goal]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter Goals" style={styles.input} />
-        <Button title="Add Goals" />
+        <TextInput
+          placeholder="Enter Goals"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goals" onPress={addGoalHandler} />
       </View>
 
       <View></View>
